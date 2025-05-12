c7d2e7ccba808ac7bb67da460f7bb21ab9e1fe95
philipheferreira <philipheferreira@gmail.com>
2025-05-12 14:10:34 -0300

    Feat: comments in project 1 and change name for project 2

--- 3-projects/1-modelo-geral-projeto/src/app/app.component.ts
+++ 3-projects/1-modelo-geral-projeto/src/app/app.component.ts
@@ -3,10 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet], // importa as funcionalidades a serem usadas
+  templateUrl: './app.component.html', // seleciona esse arquivo HTML como o arquivo marckdown do componente
+  styleUrl: './app.component.css' // Adiciona o arquivo css como o Style desse componente
 })
-export class AppComponent {
+export class AppComponent { // criar variaveis para serem utilizadas
   title = '1-modelo-geral-projeto';
 }
