2fdcfed72b178fc9f2c772422cf1ca7da600e0a1
philipheferreira <philipheferreira@gmail.com>
2025-05-12 14:30:22 -0300

    Feat: project 3 delete component html and css. edit local html

--- 3-projects/3-modelo-component-ts-centro/src/app/app.component.ts
+++ 3-projects/3-modelo-component-ts-centro/src/app/app.component.ts
@@ -8,7 +8,24 @@ import { RouterOutlet } from '@angular/router';
     <h1>{{title}}</h1>
     <p>Usando um template criado dentro do component.ts</p>
   `, // tag que cria localmente um espaço html para ser editado
-  styles:`` // cria localmente um espaço localmente para editar css
+  styles:`
+  h1 {
+    font-size: 3.125rem;
+    color: var(--gray-900);
+    font-weight: 500;
+    line-height: 100%;
+    letter-spacing: -0.125rem;
+    margin: 0;
+    font-family: "Inter Tight", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+      "Segoe UI Symbol";
+  }
+
+  p {
+    margin: 0;
+    color: var(--gray-700);
+  }
+  ` // cria localmente um espaço localmente para editar css
 })
 export class AppComponent { // criando variaveis
   title = '3-modelo-component-ts-centro';
