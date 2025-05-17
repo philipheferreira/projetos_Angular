a4045cbf52d7f52e569e28f2720ce2838e1d2d5e
philipheferreira <philipheferreira@gmail.com>
2025-05-17 15:17:01 -0300

    Feat: things i forget

--- 3-projects/5-projeto-dois-components/src/app/components/component-um/component-um.component.ts
+++ 3-projects/5-projeto-dois-components/src/app/components/component-um/component-um.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './component-um.component.html',
   styleUrl: './component-um.component.css'
 })
-export class ComponentUm {
-
-}
+export class ComponentUm {} // Edição de nome para ser feita a chamada igual como ele está definido no componente principal
