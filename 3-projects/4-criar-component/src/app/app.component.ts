c4fab80c83ddb6545a550e88f71ce19c7210fedb
philipheferreira <philipheferreira@gmail.com>
2025-05-17 12:14:35 -0300

    Feat: criando component

--- 3-projects/4-criar-component/src/app/app.component.ts
+++ 3-projects/4-criar-component/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component' // direciona o caminho para o compnent
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NewComponent], // importa o component e as bibliotecas 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
