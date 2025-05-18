43517bf5e0b3638aa609d19a563ac8cd68226703
philipheferreira <philipheferreira@gmail.com>
2025-05-18 16:40:06 -0300

    Feat: create project 6

---
+++ 3-projects/6-botao-disable/src/app/components/new-component/new-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-component',
+  imports: [],
+  templateUrl: './new-component.component.html',
+  styleUrl: './new-component.component.css'
+})
+export class NewComponentComponent {
+
+}
