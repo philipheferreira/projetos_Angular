e83aec90ef3124fb1243659e0ad4710db7ad92b9
philipheferreira <philipheferreira@gmail.com>
2025-05-18 19:26:31 -0300

    Feat: finish project 6

--- 3-projects/6-botao-disable/src/app/app.component.ts
+++ 3-projects/6-botao-disable/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- 3-projects/6-botao-disable/src/app/components/new-component/new-component.component.ts
+++ 3-projects/6-botao-disable/src/app/components/new-component/new-component.component.ts
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './new-component.component.html',
   styleUrl: './new-component.component.css'
 })
-export class NewComponentComponent {
+export class NewComponent {
+  public estadoDesabilitado = true
 
+  public mudarEstadoDesabilitado(valor: boolean){
+    if(valor === true) {
+      this.estadoDesabilitado = false
+    }else if(valor === false) {
+      this.estadoDesabilitado = true
+    }
+    return 'NaN'
+  }
+
+  public alerta(){
+    alert('Esse botão foi apertado')
+  }
 }
