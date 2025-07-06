bdfd2211fd1dbba3b148a79e8b8bffaaadc0db8a
philipheferreira <philipheferreira@gmail.com>
2025-07-06 02:30:27 -0300

    Feat: adjust some operations in to do project

--- 3-projects/15-to-do-list/src/app/components/todo/todo.component.ts
+++ 3-projects/15-to-do-list/src/app/components/todo/todo.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { Tarefa } from '../../tarefa.model'
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
+  novaTarefa: string = '';
+  tarefas: Tarefa[] = [];
 
+  adicionarTarefa() {
+    if (this.novaTarefa.trim()) {
+      this.tarefas.push({titulo: this.novaTarefa, feito: false});
+      this.novaTarefa = '';
+    }
+  }
+
+  tarefaFinalizada(tarefa: Tarefa) {
+    tarefa.feito = !tarefa.feito;
+  }
+
+  removerTarefa(index: number) {
+    this.tarefas.splice(index, 1);
+  }
 }
