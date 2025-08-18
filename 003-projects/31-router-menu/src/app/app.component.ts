9ffb6778c84261c8e7ac4579be6c2f64fdead58a
philipheferreira <philipheferreira@gmail.com>
2025-08-18 12:04:37 -0300

    Feat: corrections for function for router

--- 003-projects/31-router-menu/src/app/app.component.ts
+++ 003-projects/31-router-menu/src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { PrimeiroComponente } from './components/primeiro-componente/primeiro-componente.component';
-import { SegundoComponente } from './components/segundo-componente/segundo-componente.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PrimeiroComponente, SegundoComponente],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
