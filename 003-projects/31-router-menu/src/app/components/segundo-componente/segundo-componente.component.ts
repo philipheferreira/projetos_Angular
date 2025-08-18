f97418751da1ea7e0f8f35fa9aaece3dcddb6159
philipheferreira <philipheferreira@gmail.com>
2025-08-18 11:38:13 -0300

    Feat: create components and adjust instruction document

---
+++ 003-projects/31-router-menu/src/app/components/segundo-componente/segundo-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo-componente',
+  imports: [],
+  templateUrl: './segundo-componente.component.html',
+  styleUrl: './segundo-componente.component.css'
+})
+export class SegundoComponenteComponent {
+
+}
