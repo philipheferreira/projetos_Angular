fd4d93847eaa97bfdf898a11ca79bb8fda18d204
philipheferreira <philipheferreira@gmail.com>
2025-08-18 09:36:25 -0300

    Feat: things i forget

--- 001-components/030 - sistema-router/src/app/component/sobre/sobre.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-about',
-  imports: [],
-  templateUrl: './sobre.component.html',
-  styleUrl: './sobre.component.css'
-})
-export class SobreComponent {}
