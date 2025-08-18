85fb5e2598bbb47f3670a554dacefb7a9a9bb0ea
philipheferreira <philipheferreira@gmail.com>
2025-08-18 16:22:25 -0300

    Feat: in process for make work

--- 003-projects/31-router-system-other-way/src/app/app.routes.ts
+++ 003-projects/31-router-system-other-way/src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { PrimeiroComponente } from './components/primeiro-componente/primeiro-componente.component';
 import { SegundoComponente } from './components/segundo-componente/segundo-componente.component';
 
-export const routes: Routes = [
+export const appRoutes: Routes = [
   { path: 'primeiro-componente', component: PrimeiroComponente },
-  { path: 'segundo-componente', component: SegundoComponente }
+  { path: 'segundo-componente', component: SegundoComponente },
+  { path: '', redirectTo: '/primeiro-componente', pathMatch: 'full' },
 ];
