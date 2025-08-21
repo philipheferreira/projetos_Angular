761ff77df6386c25ec46ec4181a947a44ab631ec
philipheferreira <philipheferreira@gmail.com>
2025-08-21 16:26:46 -0300

    Feat: adjust functionalits in html and js for project todo list

--- 003-projects/15-projeto-todo-list/src/app/components/todo/todo.component.ts
+++ 003-projects/15-projeto-todo-list/src/app/components/todo/todo.component.ts
@@ -9,26 +9,35 @@ import { CommonModule } from '@angular/common';
   styleUrl: './todo.component.css'
 })
 export class todoComponent {
+
   itens: {id: number, nome: string, concluido: boolean}[] = [];
   id: number = 1;
   receberTarefa: string = '';
 
   adicionarItem = () => {
+
     this.itens.push({
+
       id: this.id,
       nome: this.receberTarefa,
       concluido: false
+
     })
+
     this.receberTarefa = '';
     this.id++;
   }
 
   getItens = (concluido: boolean) => {
+
     return this.itens.filter(item => item.concluido === concluido);
+
   }
 
   setFinalizadas = (id: number) => {
+
     const index = this.itens.findIndex(item => item.id === id);
+
     if (index !== -1) {
       const estadoAtual = this.itens[index].concluido;
       this.itens[index].concluido = !estadoAtual;
