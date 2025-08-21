153794db0a2eb309992ce63ce07e353318aa9a84
philipheferreira <philipheferreira@gmail.com>
2025-08-21 11:22:03 -0300

    Delete erros projects

--- 003-projects/31-router-system-other-way/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { PrimeiroComponente } from './components/primeiro-componente/primeiro-componente.component';
-import { SegundoComponente } from './components/segundo-componente/segundo-componente.component';
-
-export const appRoutes: Routes = [
-  { path: 'primeiro-componente', component: PrimeiroComponente },
-  { path: 'segundo-componente', component: SegundoComponente },
-  { path: '', redirectTo: '/primeiro-componente', pathMatch: 'full' },
-];
