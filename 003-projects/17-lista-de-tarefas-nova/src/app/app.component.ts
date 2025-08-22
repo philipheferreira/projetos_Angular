869940609a4d1a35b37822174b4d123e1056677e
philipheferreira <philipheferreira@gmail.com>
2025-08-22 10:35:12 -0300

    Feat: create components for to do project

--- 003-projects/17-lista-de-tarefas-nova/src/app/app.component.ts
+++ 003-projects/17-lista-de-tarefas-nova/src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // chamando a biblioteca forms dentro do angular
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, BrowserModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'lista-de-tarefas-nova';
+  novaTarefa: string = '';
+  tarefas: { titulo: string; concluido: boolean }[] = [];
+
+  adicionarTarefa(){
+    if (this.novaTarefa.trim() !== '') {
+      this.tarefas.push({ titulo: this.novaTarefa, concluido: false});
+      this.novaTarefa = '';
+    }
+  }
+
+  removerTarefa(index: number){
+    this.tarefas.splice(index, 1);
+  }
+
+  toggleConcluido(index: number) {
+    this.tarefas[index].concluido = !this.tarefas[index].concluido;
+  }
 }
