8466c6c0a6ab014b2457c04a8aee95c356209206
philipheferreira <philipheferreira@gmail.com>
2025-08-25 14:39:01 -0300

    Feat: conect compnent with main component

--- 003-projects/15-lista-de-tarefas 1 forma de fazer/src/app/app.component.ts
+++ 003-projects/15-lista-de-tarefas 1 forma de fazer/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TarefasComponent } from './components/tarefas/tarefas.component'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TarefasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
