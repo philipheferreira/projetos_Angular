8db91fc2e23e43cd1cc6dc436402fa81f974da1c
philipheferreira <philipheferreira@gmail.com>
2025-08-25 16:15:48 -0300

    Feat: adjust create componet in new list project

---
+++ 003-projects/16-lista-de-tarefas-usando-arquivo-module/src/app/components/tarefas/tarefas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarefas',
+  imports: [],
+  templateUrl: './tarefas.component.html',
+  styleUrl: './tarefas.component.css'
+})
+export class TarefasComponent {
+
+}
