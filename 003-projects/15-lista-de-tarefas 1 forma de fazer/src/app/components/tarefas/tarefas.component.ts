c0ed54c07be694bbb8ce96aac63a9168f11befba
philipheferreira <philipheferreira@gmail.com>
2025-08-25 15:02:38 -0300

    Feat: adjust from project simple to do list module 15

--- 003-projects/15-lista-de-tarefas 1 forma de fazer/src/app/components/tarefas/tarefas.component.ts
+++ 003-projects/15-lista-de-tarefas 1 forma de fazer/src/app/components/tarefas/tarefas.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // adicionando as funcionalidades de forms para a aplicação desse componente
+import { CommonModule } from '@angular/common'; // adicionando as funcionalidades dentro de BrowserModule para esse componente
 
 @Component({
   selector: 'app-tarefas',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './tarefas.component.html',
   styleUrl: './tarefas.component.css'
 })
 export class TarefasComponent {
+  tarefas: string[] = [];
+  novaTarefa: string = '';
 
+  adicionarTarefa() {
+    if (this.novaTarefa.trim()) {
+      this.tarefas.push(this.novaTarefa.trim());
+      this.novaTarefa = '';
+    }
+  }
+
+  removerTarefa( index: number ) {
+    this.tarefas.splice(index, 1);
+  }
 }
