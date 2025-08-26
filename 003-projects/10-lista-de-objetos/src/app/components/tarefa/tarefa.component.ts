01179c11e236982eb84708717ef04dc2108a20ed
philipheferreira <philipheferreira@gmail.com>
2025-08-26 17:12:28 -0300

    Feat: finish list object project

--- 003-projects/10-lista-de-objetos/src/app/components/tarefa/tarefa.component.ts
+++ 003-projects/10-lista-de-objetos/src/app/components/tarefa/tarefa.component.ts
@@ -13,5 +13,19 @@ export class TarefaComponent {
   {id: 1, titulo: 'Estudar Angular', concluida: false},
   {id: 2, titulo: 'Fazer Exercícios', concluida: true},
   {id: 3, titulo: 'Ler documentação', concluida: false}
- ]
+ ];
+
+ adicionarTarefa() {
+  const nova: Tarefa = {
+    id: this.tarefas.length + 1,
+    titulo: `Nova tarefa ${this.tarefas.length + 1}`,
+    concluida: false
+  }
+  this.tarefas.push(nova);
+ }
+
+ removerTarefa(id: number) {
+  this.tarefas = this.tarefas.filter(tarefa => tarefa.id !== id);
+ }
+
 }
