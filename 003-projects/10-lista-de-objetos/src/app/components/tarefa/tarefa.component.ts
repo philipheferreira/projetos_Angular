1fd0bba750d8cb732308bd880a07edc28717c46c
philipheferreira <philipheferreira@gmail.com>
2025-08-26 17:06:30 -0300

    Feat: adjust list

--- 003-projects/10-lista-de-objetos/src/app/components/tarefa/tarefa.component.ts
+++ 003-projects/10-lista-de-objetos/src/app/components/tarefa/tarefa.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Tarefa } from './tarefa.model'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tarefa',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tarefa.component.html',
   styleUrl: './tarefa.component.css'
 })
 export class TarefaComponent {
-
+ tarefas: Tarefa[] = [
+  {id: 1, titulo: 'Estudar Angular', concluida: false},
+  {id: 2, titulo: 'Fazer Exercícios', concluida: true},
+  {id: 3, titulo: 'Ler documentação', concluida: false}
+ ]
 }
