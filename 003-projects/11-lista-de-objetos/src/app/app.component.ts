da14366ba14f22cd9d244de06b397f2b70077ef2
philipheferreira <philipheferreira@gmail.com>
2025-08-27 11:25:51 -0300

    Feat: project 11 create component connection

--- 003-projects/11-lista-de-objetos/src/app/app.component.ts
+++ 003-projects/11-lista-de-objetos/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TarefaComponent } from './components/tarefa/tarefa.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TarefaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ 003-projects/11-lista-de-objetos/src/app/components/tarefa/tarefa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarefa',
+  imports: [],
+  templateUrl: './tarefa.component.html',
+  styleUrl: './tarefa.component.css'
+})
+export class TarefaComponent {
+
+}
