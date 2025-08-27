f5560e004fb062b06f1e0cae0a6d2673745afa61
philipheferreira <philipheferreira@gmail.com>
2025-08-27 11:35:00 -0300

    Feat: create basic list with model finish

--- 003-projects/11-lista-de-objetos/src/app/components/tarefa/tarefa.component.ts
+++ 003-projects/11-lista-de-objetos/src/app/components/tarefa/tarefa.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Tarefa } from './tarefa.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tarefa',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tarefa.component.html',
   styleUrl: './tarefa.component.css'
 })
 export class TarefaComponent {
-
+  tarefas: Tarefa[] = [ // chama o model criado para receber as tarefas criadas com base nas caracteristicas
+    { id: 1, titulo: 'Tarefa 1', concluida: false },
+    { id: 2, titulo: 'Tarefa 2', concluida: true },
+    { id: 3, titulo: 'Tarefa 3', concluida: false },
+  ];
 }
