df140c9206adbca0cd3f213b3e81d644fef5bbab
philipheferreira <philipheferreira@gmail.com>
2025-09-01 14:57:41 -0300

    Feat: create router system

--- 003-projects/40-router-system/src/app/app.routes.ts
+++ 003-projects/40-router-system/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ContatoComponent } from './components/contato/contato.component';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent }, // Rota Padrão
+  { path: 'contato', component: ContatoComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent }
+];
