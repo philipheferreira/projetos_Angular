419ae2b2bd1134ca88be91709f0aa27e184d97dd
philipheferreira <philipheferreira@gmail.com>
2025-09-01 16:16:39 -0300

    Feat: create project

--- 003-projects/42-menu-lateral-system-router/src/app/components/menu/menu.component.ts
+++ 003-projects/42-menu-lateral-system-router/src/app/components/menu/menu.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  estaAberto = false;
 
+  toggleMenu() {
+    this.estaAberto = !this.estaAberto;
+  }
 }
