493abdc51ff27df465bbf11812c85bd0dcd5b579
philipheferreira <philipheferreira@gmail.com>
2025-09-01 13:56:23 -0300

    Feat: create modals for project

---
+++ 003-projects/12-exemplo-2-modais/src/app/component/modals/modal1/modal1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal1',
+  imports: [],
+  templateUrl: './modal1.component.html',
+  styleUrl: './modal1.component.css'
+})
+export class Modal1Component {
+
+}
---
+++ 003-projects/12-exemplo-2-modais/src/app/component/modals/modal2/modal2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal2',
+  imports: [],
+  templateUrl: './modal2.component.html',
+  styleUrl: './modal2.component.css'
+})
+export class Modal2Component {
+
+}
