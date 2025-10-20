9a5d8618f3d5d746a3ded458ddf6f85399eb5b1e
philipheferreira <philipheferreira@gmail.com>
2025-10-20 11:57:59 -0300

    Feat: adjust css file and create componenteparte

---
+++ 003-projects/70-dashboard/src/app/componenteparte/componenteparte.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componenteparte',
+  imports: [],
+  templateUrl: './componenteparte.component.html',
+  styleUrl: './componenteparte.component.css'
+})
+export class ComponenteparteComponent {
+
+}
