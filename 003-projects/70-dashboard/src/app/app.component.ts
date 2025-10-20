a00853a87e76e29de47a5f6f352b8790674fb8dd
philipheferreira <philipheferreira@gmail.com>
2025-10-20 12:05:57 -0300

    Feat: integrate component in principal compot

--- 003-projects/70-dashboard/src/app/app.component.ts
+++ 003-projects/70-dashboard/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PartComponent} from './part-component/part-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ 003-projects/70-dashboard/src/app/part-component/part-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-part-component',
+  imports: [],
+  templateUrl: './part-component.component.html',
+  styleUrl: './part-component.component.css'
+})
+export class PartComponent {
+
+}
