9d151f1415f31a68da4ac577f88d13167653d918
philipheferreira <philipheferreira@gmail.com>
2025-10-22 14:05:33 -0300

    Feat: adjust inr project with basic

--- 003-projects/11-contar-com-mais-botoes/src/app/components/contador/contador.component.spec.ts
+++ 003-projects/11-contar-com-mais-botoes/src/app/components/contador/contador.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ContadorComponent } from './contador.component';
+import { Contador } from './contador.component';
 
-describe('ContadorComponent', () => {
-  let component: ContadorComponent;
-  let fixture: ComponentFixture<ContadorComponent>;
+describe('Contador', () => {
+  let component: Contador;
+  let fixture: ComponentFixture<Contador>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ContadorComponent]
+      imports: [Contador]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ContadorComponent);
+    fixture = TestBed.createComponent(Contador);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
