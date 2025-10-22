6daad3273bf737795dc6bfc86aca53368936514f
philipheferreira <philipheferreira@gmail.com>
2025-10-22 18:08:04 -0300

    Feat: finish with contador input

--- 003-projects/12-contador-com-tela-input/src/app/components/contador/contador.component.ts
+++ 003-projects/12-contador-com-tela-input/src/app/components/contador/contador.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contador',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contador.component.html',
   styleUrl: './contador.component.css'
 })
 export class Contador {
   valor = 0;
+  valorInput = 0;
 
   somar() {
-    this.valor++;
+    this.valor += this.valorInput;
+    this.valorInput = 0;
   }
 
   subtrair() {
-    this.valor--;
+    this.valor -= this.valorInput;
+    this.valorInput = 0;
   }
 
   resetar() {
