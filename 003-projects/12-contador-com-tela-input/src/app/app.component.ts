cd46724a2a9277efc109755df1b771fcd3373b19
philipheferreira <philipheferreira@gmail.com>
2025-10-22 14:30:48 -0300

    Feat: adjusts

--- 003-projects/12-contador-com-tela-input/src/app/app.component.ts
+++ 003-projects/12-contador-com-tela-input/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Contador } from './components/contador/contador.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Contador],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
