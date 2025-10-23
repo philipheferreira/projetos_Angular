be75cd7ede45a525c45b91970a22f24306f6ee7c
philipheferreira <philipheferreira@gmail.com>
2025-10-23 15:32:42 -0300

    Feat: adjust login page

--- 003-projects/60-login-page/src/app/app.component.spec.ts
+++ 003-projects/60-login-page/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'login-form' title`, () => {
+  it(`should have the 'login-page' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('login-form');
+    expect(app.title).toEqual('login-page');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, login-form');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, login-page');
   });
 });
