70b87b64a8c58754dcecf7266fd460c4fe6de631
philipheferreira <philipheferreira@gmail.com>
2025-10-29 11:04:55 -0300

    Feat: menu lateral finish

--- 003-projects/51-menu-funcional-sidebar-standalone/src/app/app.component.ts
+++ 003-projects/51-menu-funcional-sidebar-standalone/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'menu-funcional-sidebar-standalone';
+  ativarMenuLateral = false;
+
+  ativarEDesativarMenuLateral() {
+    this.ativarMenuLateral = !this.ativarMenuLateral
+  }
 }
