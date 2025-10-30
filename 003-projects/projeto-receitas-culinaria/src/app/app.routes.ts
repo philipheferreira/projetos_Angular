2667cecad5b19ae49a214f37eb3585c2b628cbfb
philipheferreira <philipheferreira@gmail.com>
2025-10-29 21:39:30 -0300

    Feat: create menu in recep project

--- 003-projects/projeto-receitas-culinaria/src/app/app.routes.ts
+++ 003-projects/projeto-receitas-culinaria/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { ReceitasComponent } from './pages/receitas/receitas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PrincipalComponent },
+  { path: 'receitas', component: ReceitasComponent },
+];
